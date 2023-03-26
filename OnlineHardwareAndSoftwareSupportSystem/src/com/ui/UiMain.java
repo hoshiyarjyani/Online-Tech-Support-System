b4a9157e486ad14b36c90a9b4729b9be27980a02
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-03-26 16:28:55 +0530

    Menu Added in UI Main

--- OnlineHardwareAndSoftwareSupportSystem/src/com/ui/UiMain.java
+++ OnlineHardwareAndSoftwareSupportSystem/src/com/ui/UiMain.java
@@ -1,9 +1,25 @@
 package com.ui;
 
-public class UiMain {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+public class UiMain {
+		public static void main(String[] args) {
+			System.out.println("Welcome to Online Hardware and Software Support System.");
+			System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+			System.out.println("========Select your Option========\n");
+			System.out.println("Press 1 if you are the HOD.");
+			System.out.println("Press 2 if you are an Employee.");
+			System.out.println("Press 3 if you are an Engineer.");
+			System.out.println("Press 4 to Stop/Exit Application");
+			
+			Scanner sc = new Scanner(System.in);
+			
+			int choice = sc.nextInt();
+			
+			System.out.println("================================");
+			
+			
+		}
 	}
 
-}
+
