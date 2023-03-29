4aa21ec793b345e06263e36f8a576b7d9a736868
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-03-29 15:12:04 +0530

    Engineer can See the Complaint Assign to him by HOD,option 1 done of Engineer

---
+++ OnlineHardwareAndSoftwareSupportSystem/src/com/ui/EngineerCheckComplaintsAssigned.java
@@ -0,0 +1,26 @@
+package com.ui;
+
+import java.util.List;
+
+import com.dao.EngineerDAO;
+import com.dao.EngineerDAOImpl;
+import com.dto.Complaints;
+import com.exception.ComplaintException;
+
+public class EngineerCheckComplaintsAssigned {
+	public void checkComplaintsAssigned(int engId) throws ComplaintException {
+
+		EngineerDAO engineerDao = new EngineerDAOImpl();
+		try {
+			List<Complaints> complaintsAssigned = engineerDao.CheckAssignedComplaintsToEngineer(engId);
+			if (complaintsAssigned.isEmpty()) {
+				System.out.println("No New Complaints Assigned.");
+			} else {
+				complaintsAssigned.forEach(x -> System.out.println(x));
+			}
+		} catch (ComplaintException e) {
+			throw new ComplaintException("No Complaints Assigned.");
+		}
+
+	}
+}
